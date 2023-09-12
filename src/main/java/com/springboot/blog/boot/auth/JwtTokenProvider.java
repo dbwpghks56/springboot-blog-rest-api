1d4e686eedcc89bb337f8bbe47b59e1253c3ba4c
wpghks <wpghks56@naver.com>
2023-09-12 14:48:18 +0900

    [feature] create jwt utils and filter

---
+++ src/main/java/com/springboot/blog/boot/auth/JwtTokenProvider.java
@@ -0,0 +1,95 @@
+package com.springboot.blog.boot.auth;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.security.SignatureException;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.token-validity-in-seconds}")
+    private long jwtExpirationDate;
+
+    // generate JWT Token
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(currentDate)
+                .setIssuer("blog")
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
+
+    // get username from JWT Token
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // validate JWT Token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+
+        } catch (ExpiredJwtException e) {
+            log.error("만료된 토큰입니다. msg=" + e.getMessage());
+            throw new ExpiredJwtException(null, null, "만료된 토큰입니다. msg=" + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("지원하지 않는 토큰 형식입니다. msg=" + e.getMessage());
+            throw new UnsupportedJwtException("지원하지 않는 토큰 형식입니다. msg=" + e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("유효하지 않는 토큰입니다. msg=" + e.getMessage());
+            throw new MalformedJwtException("유효하지 않는 토큰입니다. Refresh Token 을 활용한 재갱신이 필요합니다. msg=" + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("유효하지 않은 인자입니다. msg=" + e.getMessage());
+            throw new IllegalArgumentException("유효하지 않은 인자입니다. msg=" + e.getMessage());
+        }
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/springboot/blog/boot/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,71 @@
+package com.springboot.blog.boot.filter;
+
+import com.springboot.blog.boot.auth.JwtTokenProvider;
+import com.springboot.blog.user.service.impl.CustomUserDetailServiceImpl;
+import com.springboot.blog.user.service.impl.UserDetailsImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        // get JWT Token from Http request
+        String token = getTokenFromRequest(request);
+
+        // validate JWT Token
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            // get username from JWT Token
+            String username = jwtTokenProvider.getUsernameFromToken(token);
+
+            // get UserDetails from username
+            UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            // remove "Bearer " from token
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
